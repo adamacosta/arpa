59771bd99c78aefe5aad53f61b93f643889224d1
adamacosta <acosta.adam@gmail.com>
2015-07-19 14:15:35 -0500

    Create check for class constructor.
    Checks to ensure the keys are characters and values are numeric. Also added the @exportClass directive to the Roxygen tags.

--- arpa/R/ngram.model.R
+++ arpa/R/ngram.model.R
@@ -20,6 +20,29 @@
 # The quote marks are needed because the hash can contain spaces and '<' and '>'
 # Document this somewhere with some vignettes.
 
+check_ngram <- function(object) {
+     errors <- character()
+     check_keys <- function(keys) {
+          if (!is.character(keys)) {
+               msg <- paste('Keys are ', class(keys), '. Must be character.')
+               errors <- c(errors, msg)
+          }
+     }
+     check_vals <- function(vals) {
+          if (!is.numeric(vals)) {
+               msg <- paste('Values are ', class(vals), '. Must be numeric.')
+               errors <- c(errors, msg)
+          }
+     }
+     check_keys(keys(object@unigrams))
+     check_vals(values(object@unigrams))
+     check_keys(keys(object@bigrams))
+     check_vals(values(object@bigrams))
+     check_keys(keys(object@trigrams))
+     check_vals(values(object@trigrams))
+     if (length(errors) == 0) TRUE else errors
+}
+
 #' An S4 class to represent an ngram language model
 #'
 #' @author Adam Acosta
@@ -28,11 +51,13 @@
 #' @slot unigrams A hash table mapping unigrams to their log probability
 #' @slot bigrams A hash table mapping bigrams to their log probability
 #' @slot trigrams A hash table mapping trigrams to their log probability
+#' @exportClass
 ngram.model <- setClass("ngram.model",
          representation(unigrams="hash",
                         bigrams="hash",
                         trigrams="hash"),
          prototype(unigrams=hash(keys=c('<s>'), values=c(-99)),
                    bigrams=hash(keys=c('<s> </s>'), values=c(-99)),
-                   trigrams=hash(keys=c('<s> <unk> </s>'), values=c(-99)))
+                   trigrams=hash(keys=c('<s> <unk> </s>'), values=c(-99))),
+         validity=check_ngram
 )
