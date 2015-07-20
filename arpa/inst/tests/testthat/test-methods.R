916c4a65aff1beeedf4e18d9e7a1e5b501bb5c47
adamacosta <acosta.adam@gmail.com>
2015-07-20 06:37:15 -0500

    Remove references to hash.
    See last commit. ngram.model structure changed.

--- arpa/inst/tests/testthat/test-methods.R
+++ arpa/inst/tests/testthat/test-methods.R
@@ -1,32 +1,27 @@
 context('test methods')
 
-ukeys <- c('a', 'unigram')
-uvals <- c(log(0.2), log(0.8))
-bkeys <- c('a bigram', 'another bigram')
-bvals <- c(log(0.3), log(0.7))
-tkeys <- c('i am trigram', 'so am i')
-tvals <- c(log(0.5), log(0.5))
-
-u <- hash(keys=ukeys, values=uvals)
-b <- hash(keys=bkeys, values=bvals)
-t <- hash(keys=tkeys, values=tvals)
+u <- data.table(logp=c(log(0.2), log(0.8)), w1=c('a', 'unigram'))
+b <- data.table(logp=c(log(0.3), log(0.7)), w1=c('a', 'another'),
+                w2=c('bigram', 'bigram'))
+t <- data.table(logp=c(log(0.5), log(0.5)), w1=c('i', 'so'),
+                w2=c('am', 'am'), w3=c('trigram', 'i'))
 nm <- new('ngram.model',
           unigrams=u,
           bigrams=b,
           trigrams=t)
 
 test_that('ngram.model unigrams returned from unigrams() call', {
-     expect_is(unigrams(nm), 'hash')
+     expect_is(unigrams(nm), 'data.table')
      expect_equal(unigrams(nm), nm@unigrams)
 })
 
 test_that('ngram.model bigrams returned from bigrams() call', {
-     expect_is(bigrams(nm), 'hash')
+     expect_is(bigrams(nm), 'data.table')
      expect_equal(bigrams(nm), nm@bigrams)
 })
 
 test_that('ngram.model trigrams returned from trigrams() call', {
-     expect_is(trigrams(nm), 'hash')
+     expect_is(trigrams(nm), 'data.table')
      expect_equal(trigrams(nm), nm@trigrams)
 })
 
