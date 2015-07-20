4de1c93c6da97651e7c7b1e6aad38253f9ae3fc4
adamacosta <acosta.adam@gmail.com>
2015-07-20 06:36:50 -0500

    Changed tests to check for data.table rather than hash.
    The internal structure of ngram.model changed to support more flexible querying, rather than only keying by the entire ngram.

--- arpa/inst/tests/testthat/test-construct.R
+++ arpa/inst/tests/testthat/test-construct.R
@@ -1,32 +1,16 @@
 context('test class constructor')
 
-ukeys <- c('a', 'unigram')
-uvals <- c(log(0.2), log(0.8))
-bkeys <- c('a bigram', 'another bigram')
-bvals <- c(log(0.3), log(0.7))
-tkeys <- c('i am trigram', 'so am i')
-tvals <- c(log(0.5), log(0.5))
+u <- data.table(logp=c(log(0.2), log(0.8)), w1=c('a', 'unigram'))
+b <- data.table(logp=c(log(0.3), log(0.7)), w1=c('a', 'another'),
+                w2=c('bigram', 'bigram'))
+t <- data.table(logp=c(log(0.5), log(0.5)), w1=c('i', 'so'),
+                w2=c('am', 'am'), w3=c('trigram', 'i'))
 
-u <- hash(keys=ukeys, values=uvals)
-b <- hash(keys=bkeys, values=bvals)
-t <- hash(keys=tkeys, values=tvals)
-
-test_that('ngram.model can be constructed from hash tables', {
+test_that('ngram.model can be constructed from data.tables', {
      nm <- new('ngram.model',
                unigrams=u,
                bigrams=b,
                trigrams=t)
      expect_true(isS4(nm))
      expect_is(nm, 'ngram.model')
-     expect_equal(nm@unigrams$'a', log(0.2))
-})
-
-test_that('ngram.model can be constructed from <key:value> pairs', {
-     nm <- new('ngram.model',
-               unigrams=hash(keys=ukeys, values=uvals),
-               bigrams=hash(keys=bkeys, values=bvals),
-               trigrams=hash(keys=tkeys, values=tvals))
-     expect_true(isS4(nm))
-     expect_is(nm, 'ngram.model')
-     expect_equal(nm@unigrams$'a', log(0.2))
 })
